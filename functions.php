e4eae062130b4f7d7b0a9cb31c1db080c9cbd349
Darcy W. Christ <darcy@1000camels.com>
2013-08-09 12:02:26 +0800

    removed excerpt entirely

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 	<?php
 
 	function largo_excerpt( $post, $sentence_count = 5, $use_more = true, $more_link = '', $echo = true ) {
-		if ( is_home() && strpos( $post->post_content, '<!--more-->' ) && ( !$use_more ) ) : // if we're on the homepage and the post has a more tag, use that
+		/*if ( is_home() && strpos( $post->post_content, '<!--more-->' ) && ( !$use_more ) ) : // if we're on the homepage and the post has a more tag, use that
 			$output = '<p>' . strip_tags( get_the_content( $more_link ) ) . '</p>';
 		elseif ( $post->post_excerpt ) : // if it has the optional excerpt set, use THAT
 			//if ( !$use_more ) :
@@ -14,10 +14,13 @@
 			$output .= '<a href="' . get_permalink() . '">' . $more_link . '</a>';
 			$output = str_replace( '(more...)', '', $output );
 			$output = apply_filters( 'the_content', $output );
-		endif;
+		endif;*/
+
+		$output = get_the_content();
+		$output = apply_filters( 'the_content', $output );
 
 		if ( $echo )
 			echo $output;
 
-		return 'TEST'.$output;
+		return $output;
 	}
